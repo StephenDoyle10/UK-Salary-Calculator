9053d694aaa7cb6cd0b736941d2b7c1eee0480d3
Stephen Doyle <stephendoyle@Stephens-MacBook-Pro.local>
2021-02-20 07:52:20 +0000

    update

--- public/logic.js
+++ public/logic.js
@@ -1,142 +1,199 @@
-
-
 //function to make sure numbers display as units of money, with commas in the right place, ie 100000 displays as £100,000 and not £100000
-function thous(num)
-  {
-    var num_parts = num.toString().split(".");
-    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return num_parts.join(".");
-  };
+function thous(num) {
+  var num_parts = num.toString().split(".");
+  num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return num_parts.join(".");
+}
 
 function earnings(gross) {
-  
-
-  if(gross>0&&gross<=12500){
-
-    if (gross<=9500){
+  if (gross > 0 && gross <= 12500) {
+    if (gross <= 9500) {
       let totalNI = 0;
       let totalIT = 0;
-      let totalTax = totalNI+totalIT;
-      let yearlyGrossMinusTax = gross-totalTax;
-      let takeHomeMonthly = (gross-totalTax)/12;
-      return "Your gross annual pay (in other words, pay before tax) is <strong>£"+thous(gross.toFixed(2))+"</strong>."
-        +"<br>Your take home pay for the year is <strong>£" +thous(yearlyGrossMinusTax.toFixed(2))+ "</strong>, which works out at <strong>£" +thous(takeHomeMonthly.toFixed(2))+ "</strong> per month. "
-        +"<br>This means that over the year you will pay <strong>£"+thous(totalIT)+"</strong> in income tax, and <strong>£" +thous(totalNI) +"</strong> in National Insurance.";}
-    else {
-        let totalNI = (gross-9500)*0.12;
-        let totalIT = 0;
-        let totalTax = totalNI+totalIT;
-        let yearlyGrossMinusTax = gross-totalTax;
-        let takeHomeMonthly = (gross-totalTax)/12;
-        return "Your gross annual pay (in other words, pay before tax) is <strong>£"+thous(gross.toFixed(2))+"</strong>."
-          +"<br>Your take home pay for the year is <strong>£" +thous(yearlyGrossMinusTax.toFixed(2))+ "</strong>, which works out at <strong>£" +thous(takeHomeMonthly.toFixed(2))+ "</strong> per month. "
-          +"<br>This means that over the year you will pay <strong>£"+thous(totalIT)+"</strong> in income tax, and <strong>£" +thous(totalNI) +"</strong> in National Insurance.";}
-}
+      let totalTax = totalNI + totalIT;
+      let yearlyGrossMinusTax = gross - totalTax;
+      let takeHomeMonthly = (gross - totalTax) / 12;
+      return (
+        "Your gross annual pay (in other words, pay before tax) is <strong>£" +
+        thous(gross.toFixed(2)) +
+        "</strong>." +
+        "<br>Your take home pay for the year is <strong>£" +
+        thous(yearlyGrossMinusTax.toFixed(2)) +
+        "</strong>, which works out at <strong>£" +
+        thous(takeHomeMonthly.toFixed(2)) +
+        "</strong> per month. " +
+        "<br>This means that over the year you will pay <strong>£" +
+        thous(totalIT) +
+        "</strong> in income tax, and <strong>£" +
+        thous(totalNI) +
+        "</strong> in National Insurance."
+      );
+    } else {
+      let totalNI = (gross - 9500) * 0.12;
+      let totalIT = 0;
+      let totalTax = totalNI + totalIT;
+      let yearlyGrossMinusTax = gross - totalTax;
+      let takeHomeMonthly = (gross - totalTax) / 12;
+      return (
+        "Your gross annual pay (in other words, pay before tax) is <strong>£" +
+        thous(gross.toFixed(2)) +
+        "</strong>." +
+        "<br>Your take home pay for the year is <strong>£" +
+        thous(yearlyGrossMinusTax.toFixed(2)) +
+        "</strong>, which works out at <strong>£" +
+        thous(takeHomeMonthly.toFixed(2)) +
+        "</strong> per month. " +
+        "<br>This means that over the year you will pay <strong>£" +
+        thous(totalIT) +
+        "</strong> in income tax, and <strong>£" +
+        thous(totalNI) +
+        "</strong> in National Insurance."
+      );
+    }
+  } else if (gross > 12500 && gross <= 50000) {
+    let totalNI = (gross - 9500) * 0.12;
+    let totalIT = (gross - 12500) * 0.2;
+    let totalTax = totalNI + totalIT;
+    let yearlyGrossMinusTax = gross - totalTax;
+    let takeHomeMonthly = (gross - totalTax) / 12;
 
-  else if (gross>12500&&gross<=50000){
-      let totalNI = (gross-9500)*0.12;
-      let totalIT= (gross-12500)*0.2;
-      let totalTax = totalNI+totalIT;
-      let yearlyGrossMinusTax = gross-totalTax;
-      let takeHomeMonthly = (gross-totalTax)/12;
+    return (
+      "Your gross annual pay (in other words, pay before tax) is <strong>£" +
+      thous(gross.toFixed(2)) +
+      "</strong>." +
+      "<br>Your take home pay for the year is <strong>£" +
+      thous(yearlyGrossMinusTax.toFixed(2)) +
+      "</strong>, which works out at <strong>£" +
+      thous(takeHomeMonthly.toFixed(2)) +
+      "</strong> per month. " +
+      "<br>This means that over the year you will pay <strong>£" +
+      thous(totalIT) +
+      "</strong> in income tax, and <strong>£" +
+      thous(totalNI) +
+      "</strong> in National Insurance."
+    );
+  } else if (gross > 50000 && gross <= 100001) {
+    let niTaxBand1 = 4860;
+    let niTaxBand2 = (gross - 50000) * 0.02;
+    let totalNI = niTaxBand1 + niTaxBand2;
+    let incomeTaxBand1 = 7500;
+    let incomeTaxBand2 = (gross - 50000) * 0.4;
+    let totalIT = incomeTaxBand1 + incomeTaxBand2;
+    let totalTax = totalNI + totalIT;
+    let yearlyGrossMinusTax = gross - totalTax;
+    let takeHomeMonthly = (gross - totalTax) / 12;
 
-    return "Your gross annual pay (in other words, pay before tax) is <strong>£"+thous(gross.toFixed(2))+"</strong>."
-          +"<br>Your take home pay for the year is <strong>£" +thous(yearlyGrossMinusTax.toFixed(2))+ "</strong>, which works out at <strong>£" +thous(takeHomeMonthly.toFixed(2))+ "</strong> per month. "
-          +"<br>This means that over the year you will pay <strong>£"+thous(totalIT)+"</strong> in income tax, and <strong>£" +thous(totalNI) +"</strong> in National Insurance.";}
-  
+    return (
+      "Your gross annual pay (in other words, pay before tax) is <strong>£" +
+      thous(gross.toFixed(2)) +
+      "</strong>." +
+      "<br>Your take home pay for the year is <strong>£" +
+      thous(yearlyGrossMinusTax.toFixed(2)) +
+      "</strong>, which works out at <strong>£" +
+      thous(takeHomeMonthly.toFixed(2)) +
+      "</strong> per month. " +
+      "<br>This means that over the year you will pay <strong>£" +
+      thous(totalIT) +
+      "</strong> in income tax, and <strong>£" +
+      thous(totalNI) +
+      "</strong> in National Insurance."
+    );
+  } else if (gross > 100001 && gross <= 125000) {
+    let personalAllowance = 12500 - (gross - 100000) / 2;
+    let personalAllowanceReduction = 12500 - personalAllowance;
+    let incomeTaxBand1 = 7500;
+    let incomeTaxBand2 = (gross - (50000 - personalAllowanceReduction)) * 0.4;
+    let totalIT = incomeTaxBand1 + incomeTaxBand2;
+    let niTaxBand1 = 4860;
+    let niTaxBand2 = (gross - 50000) * 0.02;
+    let totalNI = niTaxBand1 + niTaxBand2;
+    let totalTax = totalIT + totalNI;
+    let yearlyGrossMinusTax = gross - totalTax;
+    let takeHomeMonthly = (gross - totalTax) / 12;
 
+    return (
+      "Your gross annual pay (in other words, pay before tax) is <strong>£" +
+      thous(gross.toFixed(2)) +
+      "</strong>." +
+      "<br>Your take home pay for the year is <strong>£" +
+      thous(yearlyGrossMinusTax.toFixed(2)) +
+      "</strong>, which works out at <strong>£" +
+      thous(takeHomeMonthly.toFixed(2)) +
+      "</strong> per month. " +
+      "<br>This means that over the year you will pay <strong>£" +
+      thous(totalIT) +
+      "</strong> in income tax, and <strong>£" +
+      thous(totalNI) +
+      "</strong> in National Insurance."
+    );
+  } else if (gross > 125000 && gross <= 150000) {
+    let niTaxBand1 = 4860;
+    let niTaxBand2 = (gross - 50000) * 0.02;
+    let totalNI = niTaxBand1 + niTaxBand2;
+    let incomeTaxBand1 = 7500;
+    let incomeTaxBand2 = (gross - 37500) * 0.4;
+    let totalIT = incomeTaxBand1 + incomeTaxBand2;
+    let totalTax = totalNI + totalIT;
+    let yearlyGrossMinusTax = gross - totalTax;
+    let takeHomeMonthly = (gross - totalTax) / 12;
 
+    return (
+      "Your gross annual pay (in other words, pay before tax) is <strong>£" +
+      thous(gross.toFixed(2)) +
+      "</strong>." +
+      "<br>Your take home pay for the year is <strong>£" +
+      thous(yearlyGrossMinusTax.toFixed(2)) +
+      "</strong>, which works out at <strong>£" +
+      thous(takeHomeMonthly.toFixed(2)) +
+      "</strong> per month. " +
+      "<br>This means that over the year you will pay <strong>£" +
+      thous(totalIT) +
+      "</strong> in income tax, and <strong>£" +
+      thous(totalNI) +
+      "</strong> in National Insurance."
+    );
+  } else if (gross > 150000) {
+    let niTaxBand1 = 4860;
+    let niTaxBand2 = (gross - 50000) * 0.02;
+    let totalNI = niTaxBand1 + niTaxBand2;
+    let incomeTaxBand1 = 7500;
+    let incomeTaxBand2 = 45000;
+    let incomeTaxBand3 = (gross - 150000) * 0.45;
+    let totalIT = incomeTaxBand1 + incomeTaxBand2 + incomeTaxBand3;
+    let totalTax = totalNI + totalIT;
+    let yearlyGrossMinusTax = gross - totalTax;
+    let takeHomeMonthly = (gross - totalTax) / 12;
 
-  else if (gross>50000&&gross<=100001){
-      let niTaxBand1 = 4860;
-      let niTaxBand2 = (gross-50000)*0.02;
-      let totalNI = niTaxBand1+niTaxBand2;
-      let incomeTaxBand1 = 7500;
-      let incomeTaxBand2 = (gross-50000)*0.4;
-      let totalIT = incomeTaxBand1+incomeTaxBand2;
-      let totalTax = totalNI+totalIT;
-      let yearlyGrossMinusTax = gross-totalTax;
-      let takeHomeMonthly = (gross-totalTax)/12;
-
-
-    return "Your gross annual pay (in other words, pay before tax) is <strong>£"+thous(gross.toFixed(2))+"</strong>."
-          +"<br>Your take home pay for the year is <strong>£" +thous(yearlyGrossMinusTax.toFixed(2))+ "</strong>, which works out at <strong>£" +thous(takeHomeMonthly.toFixed(2))+ "</strong> per month. "
-          +"<br>This means that over the year you will pay <strong>£"+thous(totalIT)+"</strong> in income tax, and <strong>£" +thous(totalNI) +"</strong> in National Insurance.";
+    return (
+      "Your gross annual pay (in other words, pay before tax) is <strong>£" +
+      thous(gross.toFixed(2)) +
+      "</strong>." +
+      "<br>Your take home pay for the year is <strong>£" +
+      thous(yearlyGrossMinusTax.toFixed(2)) +
+      "</strong>, which works out at <strong>£" +
+      thous(takeHomeMonthly.toFixed(2)) +
+      "</strong> per month. " +
+      "<br>This means that over the year you will pay <strong>£" +
+      thous(totalIT) +
+      "</strong> in income tax, and <strong>£" +
+      thous(totalNI) +
+      "</strong> in National Insurance."
+    );
+  } else if (gross <= 0) {
+    return "Error. Illegible amount entered.";
+  } else {
+    return "Error. Illegible amount entered.";
   }
-    
-
-    else if (gross>100001&&gross<=125000){
-      let personalAllowance = 12500-((gross-100000)/2);
-      let personalAllowanceReduction = 12500-personalAllowance;
-      let incomeTaxBand1 = 7500;
-      let incomeTaxBand2 = (gross-(50000-personalAllowanceReduction))*0.4;
-      let totalIT = incomeTaxBand1+incomeTaxBand2;
-      let niTaxBand1 = 4860;
-      let niTaxBand2 = (gross-50000)*0.02;
-      let totalNI = niTaxBand1+niTaxBand2;
-      let totalTax = totalIT + totalNI;
-      let yearlyGrossMinusTax = gross-totalTax;
-      let takeHomeMonthly = (gross-totalTax)/12;
-
-      return "Your gross annual pay (in other words, pay before tax) is <strong>£"+thous(gross.toFixed(2))+"</strong>."
-          +"<br>Your take home pay for the year is <strong>£" +thous(yearlyGrossMinusTax.toFixed(2))+ "</strong>, which works out at <strong>£" +thous(takeHomeMonthly.toFixed(2))+ "</strong> per month. "
-          +"<br>This means that over the year you will pay <strong>£"+thous(totalIT)+"</strong> in income tax, and <strong>£" +thous(totalNI) +"</strong> in National Insurance." 
-      ;
-
-    }
-    else if (gross>125000&&gross<=150000){
-      let niTaxBand1=4860;
-      let niTaxBand2=(gross-50000)*0.02;
-      let totalNI = niTaxBand1+niTaxBand2;
-      let incomeTaxBand1 = 7500;
-      let incomeTaxBand2 = (gross-37500)*0.4;
-      let totalIT = incomeTaxBand1+incomeTaxBand2;
-      let totalTax = totalNI + totalIT;
-      let yearlyGrossMinusTax = gross-totalTax;
-      let takeHomeMonthly = (gross-totalTax)/12;
-
-      return "Your gross annual pay (in other words, pay before tax) is <strong>£"+thous(gross.toFixed(2))+"</strong>."
-          +"<br>Your take home pay for the year is <strong>£" +thous(yearlyGrossMinusTax.toFixed(2))+ "</strong>, which works out at <strong>£" +thous(takeHomeMonthly.toFixed(2))+ "</strong> per month. "
-          +"<br>This means that over the year you will pay <strong>£"+thous(totalIT)+"</strong> in income tax, and <strong>£" +thous(totalNI) +"</strong> in National Insurance." 
-      ;
-
-
-      
-
-    }else if (gross>150000){
-      let niTaxBand1=4860;
-      let niTaxBand2=(gross-50000)*0.02;
-      let totalNI = niTaxBand1+niTaxBand2;
-      let incomeTaxBand1 = 7500;
-      let incomeTaxBand2 = 45000;
-      let incomeTaxBand3 = (gross-150000)*0.45;
-      let totalIT = incomeTaxBand1+incomeTaxBand2+incomeTaxBand3;
-      let totalTax = totalNI + totalIT;
-      let yearlyGrossMinusTax = gross-totalTax;
-      let takeHomeMonthly = (gross-totalTax)/12;
-
-      return "Your gross annual pay (in other words, pay before tax) is <strong>£"+thous(gross.toFixed(2))+"</strong>."
-          +"<br>Your take home pay for the year is <strong>£" +thous(yearlyGrossMinusTax.toFixed(2))+ "</strong>, which works out at <strong>£" +thous(takeHomeMonthly.toFixed(2))+ "</strong> per month. "
-          +"<br>This means that over the year you will pay <strong>£"+thous(totalIT)+"</strong> in income tax, and <strong>£" +thous(totalNI) +"</strong> in National Insurance." 
-      ;
-
-
-    }
-  else if(gross<=0){return "Error. Illegible amount entered.";}
-  else {return "Error. Illegible amount entered.";}
-
-};
-
-function htmlEarnings(){
-  var gross = parseInt(document.getElementById("salCalc").elements.namedItem("name1").value);
-  //var y = parseInt(document.getElementById("replace").elements.namedItem("name2").value);
-  //var z = parseInt(document.getElementById("replace").elements.namedItem("name3").value);
-  event.preventDefault();
-  document.getElementById("result").innerHTML = earnings(gross)
-
 }
 
-
+function htmlEarnings() {
+  var gross = parseInt(
+    document.getElementById("salCalc").elements.namedItem("name1").value
+  );
+  event.preventDefault();
+  document.getElementById("result").innerHTML = earnings(gross);
+}
 
 exports.thous = thous;
 exports.earnings = earnings;
+
--- test/index_test.js
+++ test/index_test.js
@@ -1,151 +1,110 @@
-const assert = require('assert');
-const {thous, earnings} = require('../public/logic.js');
+const assert = require("assert");
+const { thous, earnings } = require("../public/logic.js");
 
+describe("thous", () => {
+  it("to make sure numbers display with commas in the right place so they look more like money values, ie 100000 displays as 100,000, and 67222 displays as 67,222", () => {
+    const result = thous(1000);
+    const expected = "1,000";
 
-
-describe('thous', () => {
-  it('to make sure numbers display with commas in the right place so they look more like money values, ie 100000 displays as 100,000, and 67222 displays as 67,222', () => {
-
-
-  
-    const tester = thous(1000)
-  const expectedResult = "1,000";
-  
-    assert.ok(tester === expectedResult);
-    
+    assert.strictEqual(result,expected);
   });
 });
 
+describe("earnings -£1000", () => {
+  it("convert an annual salary of -1000 into a string that shows how such a salary is broken into different taxes", () => {
+    const result = earnings(-1000);
+    const expected = "Error. Illegible amount entered.";
 
-describe('earnings -£1000', () => {
-  it('convert an annual salary of -1000 into a string that shows how such a salary is broken into different taxes', () => {
-
-
-  
-    const tester = earnings(-1000)
-  const expectedResult = "Error. Illegible amount entered.";
-  
-    assert.ok(tester === expectedResult);
-    
+    assert.ok(result === expected);
   });
 });
 
+describe("earnings £0", () => {
+  it("convert an annual salary of £0 into a string that shows how such a salary is broken into different taxes", () => {
+    const result = earnings(0);
+    const expected = "Error. Illegible amount entered.";
 
-describe('earnings £0', () => {
-  it('convert an annual salary of £0 into a string that shows how such a salary is broken into different taxes', () => {
-
-
-  
-    const tester = earnings(0)
-  const expectedResult = "Error. Illegible amount entered.";
-  
-    assert.ok(tester === expectedResult);
-    
+    assert.ok(result === expected);
   });
 });
 
-describe('earnings £10,000', () => {
-  it('convert an annual salary of £10,000 into a string that shows how such a salary is broken into different taxes', () => {
+describe("earnings £10,000", () => {
+  it("convert an annual salary of £10,000 into a string that shows how such a salary is broken into different taxes", () => {
+    const result = earnings(10000);
+    const expected =
+      "Your gross annual pay (in other words, pay before tax) is <strong>£10,000.00</strong>.<br>Your take home pay for the year is <strong>£9,940.00</strong>, which works out at <strong>£828.33</strong> per month. <br>This means that over the year you will pay <strong>£0</strong> in income tax, and <strong>£60</strong> in National Insurance.";
 
-
-  
-    const tester = earnings(10000)
-  const expectedResult = "Your gross annual pay (in other words, pay before tax) is <strong>£10,000.00</strong>.<br>Your take home pay for the year is <strong>£9,940.00</strong>, which works out at <strong>£828.33</strong> per month. <br>This means that over the year you will pay <strong>£0</strong> in income tax, and <strong>£60</strong> in National Insurance.";
-  
-    assert.ok(tester === expectedResult);
-    
+    assert.ok(result === expected);
   });
 });
 
-describe('earnings £25,000', () => {
-  it('convert an annual salary of £25,000 into a string that shows how such a salary is broken into different taxes', () => {
+describe("earnings £25,000", () => {
+  it("convert an annual salary of £25,000 into a string that shows how such a salary is broken into different taxes", () => {
+    const result = earnings(25000);
+    const expected =
+      "Your gross annual pay (in other words, pay before tax) is <strong>£25,000.00</strong>.<br>Your take home pay for the year is <strong>£20,640.00</strong>, which works out at <strong>£1,720.00</strong> per month. <br>This means that over the year you will pay <strong>£2,500</strong> in income tax, and <strong>£1,860</strong> in National Insurance.";
 
-
-  
-    const tester = earnings(25000)
-  const expectedResult = "Your gross annual pay (in other words, pay before tax) is <strong>£25,000.00</strong>.<br>Your take home pay for the year is <strong>£20,640.00</strong>, which works out at <strong>£1,720.00</strong> per month. <br>This means that over the year you will pay <strong>£2,500</strong> in income tax, and <strong>£1,860</strong> in National Insurance.";
-  
-    assert.ok(tester === expectedResult);
-    
+    assert.ok(result === expected);
   });
 });
 
-describe('earnings £50,000', () => {
-  it('convert an annual salary of £50,000 into a string that shows how such a salary is broken into different taxes', () => {
+describe("earnings £50,000", () => {
+  it("convert an annual salary of £50,000 into a string that shows how such a salary is broken into different taxes", () => {
+    const result = earnings(50000);
+    const expected =
+      "Your gross annual pay (in other words, pay before tax) is <strong>£50,000.00</strong>.<br>Your take home pay for the year is <strong>£37,640.00</strong>, which works out at <strong>£3,136.67</strong> per month. <br>This means that over the year you will pay <strong>£7,500</strong> in income tax, and <strong>£4,860</strong> in National Insurance.";
 
-
-  
-    const tester = earnings(50000)
-  const expectedResult = "Your gross annual pay (in other words, pay before tax) is <strong>£50,000.00</strong>.<br>Your take home pay for the year is <strong>£37,640.00</strong>, which works out at <strong>£3,136.67</strong> per month. <br>This means that over the year you will pay <strong>£7,500</strong> in income tax, and <strong>£4,860</strong> in National Insurance.";
-  
-    assert.ok(tester === expectedResult);
-    
+    assert.ok(result === expected);
   });
 });
 
-describe('earnings £75,000', () => {
-  it('convert an annual salary of £75,000 into a string that shows how such a salary is broken into different taxes', () => {
+describe("earnings £75,000", () => {
+  it("convert an annual salary of £75,000 into a string that shows how such a salary is broken into different taxes", () => {
+    const result = earnings(75000);
+    const expected =
+      "Your gross annual pay (in other words, pay before tax) is <strong>£75,000.00</strong>.<br>Your take home pay for the year is <strong>£52,140.00</strong>, which works out at <strong>£4,345.00</strong> per month. <br>This means that over the year you will pay <strong>£17,500</strong> in income tax, and <strong>£5,360</strong> in National Insurance.";
 
-
-  
-    const tester = earnings(75000)
-  const expectedResult = "Your gross annual pay (in other words, pay before tax) is <strong>£75,000.00</strong>.<br>Your take home pay for the year is <strong>£52,140.00</strong>, which works out at <strong>£4,345.00</strong> per month. <br>This means that over the year you will pay <strong>£17,500</strong> in income tax, and <strong>£5,360</strong> in National Insurance.";
-  
-    assert.ok(tester === expectedResult);
-    
+    assert.ok(result === expected);
   });
 });
 
-describe('earnings £100,000', () => {
-  it('convert an annual salary of £100,000 into a string that shows how such a salary is broken into different taxes', () => {
+describe("earnings £100,000", () => {
+  it("convert an annual salary of £100,000 into a string that shows how such a salary is broken into different taxes", () => {
+    const result = earnings(100000);
+    const expected =
+      "Your gross annual pay (in other words, pay before tax) is <strong>£100,000.00</strong>.<br>Your take home pay for the year is <strong>£66,640.00</strong>, which works out at <strong>£5,553.33</strong> per month. <br>This means that over the year you will pay <strong>£27,500</strong> in income tax, and <strong>£5,860</strong> in National Insurance.";
 
-
-  
-    const tester = earnings(100000)
-  const expectedResult = "Your gross annual pay (in other words, pay before tax) is <strong>£100,000.00</strong>.<br>Your take home pay for the year is <strong>£66,640.00</strong>, which works out at <strong>£5,553.33</strong> per month. <br>This means that over the year you will pay <strong>£27,500</strong> in income tax, and <strong>£5,860</strong> in National Insurance.";
-  
-    assert.ok(tester === expectedResult);
-    
+    assert.ok(result === expected);
   });
 });
 
-describe('earnings £150,000', () => {
-  it('convert an annual salary of £150,000 into a string that shows how such a salary is broken into different taxes', () => {
+describe("earnings £150,000", () => {
+  it("convert an annual salary of £150,000 into a string that shows how such a salary is broken into different taxes", () => {
+    const result = earnings(150000);
+    const expected =
+      "Your gross annual pay (in other words, pay before tax) is <strong>£150,000.00</strong>.<br>Your take home pay for the year is <strong>£90,640.00</strong>, which works out at <strong>£7,553.33</strong> per month. <br>This means that over the year you will pay <strong>£52,500</strong> in income tax, and <strong>£6,860</strong> in National Insurance.";
 
-
-  
-    const tester = earnings(150000)
-  const expectedResult = "Your gross annual pay (in other words, pay before tax) is <strong>£150,000.00</strong>.<br>Your take home pay for the year is <strong>£90,640.00</strong>, which works out at <strong>£7,553.33</strong> per month. <br>This means that over the year you will pay <strong>£52,500</strong> in income tax, and <strong>£6,860</strong> in National Insurance.";
-  
-    assert.ok(tester === expectedResult);
-    
+    assert.ok(result === expected);
   });
 });
 
-describe('earnings £200,000', () => {
-  it('convert an annual salary of £200,000 into a string that shows how such a salary is broken into different taxes', () => {
+describe("earnings £200,000", () => {
+  it("convert an annual salary of £200,000 into a string that shows how such a salary is broken into different taxes", () => {
+    const result = earnings(200000);
+    const expected =
+      "Your gross annual pay (in other words, pay before tax) is <strong>£200,000.00</strong>.<br>Your take home pay for the year is <strong>£117,140.00</strong>, which works out at <strong>£9,761.67</strong> per month. <br>This means that over the year you will pay <strong>£75,000</strong> in income tax, and <strong>£7,860</strong> in National Insurance.";
 
-
-  
-    const tester = earnings(200000)
-  const expectedResult = "Your gross annual pay (in other words, pay before tax) is <strong>£200,000.00</strong>.<br>Your take home pay for the year is <strong>£117,140.00</strong>, which works out at <strong>£9,761.67</strong> per month. <br>This means that over the year you will pay <strong>£75,000</strong> in income tax, and <strong>£7,860</strong> in National Insurance.";
-  
-    assert.ok(tester === expectedResult);
-    
+    assert.ok(result === expected);
   });
 });
 
-describe('earnings £500,000', () => {
-  it('convert an annual salary of £500,000 into a string that shows how such a salary is broken into different taxes', () => {
+describe("earnings £500,000", () => {
+  it("convert an annual salary of £500,000 into a string that shows how such a salary is broken into different taxes", () => {
+    const result = earnings(500000);
+    const expected =
+      "Your gross annual pay (in other words, pay before tax) is <strong>£500,000.00</strong>.<br>Your take home pay for the year is <strong>£276,140.00</strong>, which works out at <strong>£23,011.67</strong> per month. <br>This means that over the year you will pay <strong>£210,000</strong> in income tax, and <strong>£13,860</strong> in National Insurance.";
 
-
-  
-    const tester = earnings(500000)
-  const expectedResult = "Your gross annual pay (in other words, pay before tax) is <strong>£500,000.00</strong>.<br>Your take home pay for the year is <strong>£276,140.00</strong>, which works out at <strong>£23,011.67</strong> per month. <br>This means that over the year you will pay <strong>£210,000</strong> in income tax, and <strong>£13,860</strong> in National Insurance.";
-  
-    assert.ok(tester === expectedResult);
-    
+    assert.ok(result === expected);
   });
 });
-
 
