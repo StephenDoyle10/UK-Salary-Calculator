dcb16cea16ac31a4d6b42a830b1b235a503b6c31
Stephen Doyle <stephendoyle@Stephens-MacBook-Pro.local>
2021-03-05 12:13:31 +0000

    update

--- public/logic.js
+++ public/logic.js
@@ -1,3 +1,5 @@
+
+
 //function to make sure numbers display as units of money, with commas in the right place, ie 100000 displays as £100,000 and not £100000
 function thous(num) {
   var num_parts = num.toString().split(".");
@@ -6,181 +8,79 @@ function thous(num) {
 }
 
 function earnings(gross) {
+  const outputTotal = (totalIT, totalNI) => {
+    const totalTax = totalNI + totalIT;
+    const yearlyGrossMinusTax = gross - totalTax;
+    const takeHomeMonthly = (gross - totalTax) / 12;
+    return (
+      "Your gross annual pay (in other words, pay before tax) is <strong>£" +
+      thous(gross.toFixed(2)) +
+      "</strong>.<br>Your take home pay for the year is <strong>£" +
+      thous(yearlyGrossMinusTax.toFixed(2)) +
+      "</strong>, which works out at <strong>£" +
+      thous(takeHomeMonthly.toFixed(2)) +
+      "</strong> per month.<br>This means that over the year you will pay <strong>£" +
+      thous(totalIT) +
+      "</strong> in income tax, and <strong>£" +
+      thous(totalNI) +
+      "</strong> in National Insurance."
+    );
+  };
   if (gross > 0 && gross <= 12500) {
     if (gross <= 9500) {
-      let totalNI = 0;
-      let totalIT = 0;
-      let totalTax = totalNI + totalIT;
-      let yearlyGrossMinusTax = gross - totalTax;
-      let takeHomeMonthly = (gross - totalTax) / 12;
-      return (
-        "Your gross annual pay (in other words, pay before tax) is <strong>£" +
-        thous(gross.toFixed(2)) +
-        "</strong>." +
-        "<br>Your take home pay for the year is <strong>£" +
-        thous(yearlyGrossMinusTax.toFixed(2)) +
-        "</strong>, which works out at <strong>£" +
-        thous(takeHomeMonthly.toFixed(2)) +
-        "</strong> per month. " +
-        "<br>This means that over the year you will pay <strong>£" +
-        thous(totalIT) +
-        "</strong> in income tax, and <strong>£" +
-        thous(totalNI) +
-        "</strong> in National Insurance."
-      );
+      const totalNI = 0;
+      const totalIT = 0;
+
+      return outputTotal(totalIT, totalNI);
     } else {
-      let totalNI = (gross - 9500) * 0.12;
-      let totalIT = 0;
-      let totalTax = totalNI + totalIT;
-      let yearlyGrossMinusTax = gross - totalTax;
-      let takeHomeMonthly = (gross - totalTax) / 12;
-      return (
-        "Your gross annual pay (in other words, pay before tax) is <strong>£" +
-        thous(gross.toFixed(2)) +
-        "</strong>." +
-        "<br>Your take home pay for the year is <strong>£" +
-        thous(yearlyGrossMinusTax.toFixed(2)) +
-        "</strong>, which works out at <strong>£" +
-        thous(takeHomeMonthly.toFixed(2)) +
-        "</strong> per month. " +
-        "<br>This means that over the year you will pay <strong>£" +
-        thous(totalIT) +
-        "</strong> in income tax, and <strong>£" +
-        thous(totalNI) +
-        "</strong> in National Insurance."
-      );
+      const totalNI = (gross - 9500) * 0.12;
+      const totalIT = 0;
+      return outputTotal(totalIT, totalNI);
     }
   } else if (gross > 12500 && gross <= 50000) {
-    let totalNI = (gross - 9500) * 0.12;
-    let totalIT = (gross - 12500) * 0.2;
-    let totalTax = totalNI + totalIT;
-    let yearlyGrossMinusTax = gross - totalTax;
-    let takeHomeMonthly = (gross - totalTax) / 12;
-
-    return (
-      "Your gross annual pay (in other words, pay before tax) is <strong>£" +
-      thous(gross.toFixed(2)) +
-      "</strong>." +
-      "<br>Your take home pay for the year is <strong>£" +
-      thous(yearlyGrossMinusTax.toFixed(2)) +
-      "</strong>, which works out at <strong>£" +
-      thous(takeHomeMonthly.toFixed(2)) +
-      "</strong> per month. " +
-      "<br>This means that over the year you will pay <strong>£" +
-      thous(totalIT) +
-      "</strong> in income tax, and <strong>£" +
-      thous(totalNI) +
-      "</strong> in National Insurance."
-    );
+    const totalNI = (gross - 9500) * 0.12;
+    const totalIT = (gross - 12500) * 0.2;
+    return outputTotal(totalIT, totalNI);
   } else if (gross > 50000 && gross <= 100001) {
-    let niTaxBand1 = 4860;
-    let niTaxBand2 = (gross - 50000) * 0.02;
-    let totalNI = niTaxBand1 + niTaxBand2;
-    let incomeTaxBand1 = 7500;
-    let incomeTaxBand2 = (gross - 50000) * 0.4;
-    let totalIT = incomeTaxBand1 + incomeTaxBand2;
-    let totalTax = totalNI + totalIT;
-    let yearlyGrossMinusTax = gross - totalTax;
-    let takeHomeMonthly = (gross - totalTax) / 12;
+    const niTaxBand1 = 4860;
+    const niTaxBand2 = (gross - 50000) * 0.02;
+    const totalNI = niTaxBand1 + niTaxBand2;
+    const incomeTaxBand1 = 7500;
+    const incomeTaxBand2 = (gross - 50000) * 0.4;
+    const totalIT = incomeTaxBand1 + incomeTaxBand2;
 
-    return (
-      "Your gross annual pay (in other words, pay before tax) is <strong>£" +
-      thous(gross.toFixed(2)) +
-      "</strong>." +
-      "<br>Your take home pay for the year is <strong>£" +
-      thous(yearlyGrossMinusTax.toFixed(2)) +
-      "</strong>, which works out at <strong>£" +
-      thous(takeHomeMonthly.toFixed(2)) +
-      "</strong> per month. " +
-      "<br>This means that over the year you will pay <strong>£" +
-      thous(totalIT) +
-      "</strong> in income tax, and <strong>£" +
-      thous(totalNI) +
-      "</strong> in National Insurance."
-    );
+    return outputTotal(totalIT, totalNI);
   } else if (gross > 100001 && gross <= 125000) {
-    let personalAllowance = 12500 - (gross - 100000) / 2;
-    let personalAllowanceReduction = 12500 - personalAllowance;
-    let incomeTaxBand1 = 7500;
-    let incomeTaxBand2 = (gross - (50000 - personalAllowanceReduction)) * 0.4;
-    let totalIT = incomeTaxBand1 + incomeTaxBand2;
-    let niTaxBand1 = 4860;
-    let niTaxBand2 = (gross - 50000) * 0.02;
-    let totalNI = niTaxBand1 + niTaxBand2;
-    let totalTax = totalIT + totalNI;
-    let yearlyGrossMinusTax = gross - totalTax;
-    let takeHomeMonthly = (gross - totalTax) / 12;
+    const personalAllowance = 12500 - (gross - 100000) / 2;
+    const personalAllowanceReduction = 12500 - personalAllowance;
+    const incomeTaxBand1 = 7500;
+    const incomeTaxBand2 = (gross - (50000 - personalAllowanceReduction)) * 0.4;
+    const totalIT = incomeTaxBand1 + incomeTaxBand2;
+    const niTaxBand1 = 4860;
+    const niTaxBand2 = (gross - 50000) * 0.02;
+    const totalNI = niTaxBand1 + niTaxBand2;
 
-    return (
-      "Your gross annual pay (in other words, pay before tax) is <strong>£" +
-      thous(gross.toFixed(2)) +
-      "</strong>." +
-      "<br>Your take home pay for the year is <strong>£" +
-      thous(yearlyGrossMinusTax.toFixed(2)) +
-      "</strong>, which works out at <strong>£" +
-      thous(takeHomeMonthly.toFixed(2)) +
-      "</strong> per month. " +
-      "<br>This means that over the year you will pay <strong>£" +
-      thous(totalIT) +
-      "</strong> in income tax, and <strong>£" +
-      thous(totalNI) +
-      "</strong> in National Insurance."
-    );
+    return outputTotal(totalIT, totalNI);
   } else if (gross > 125000 && gross <= 150000) {
-    let niTaxBand1 = 4860;
-    let niTaxBand2 = (gross - 50000) * 0.02;
-    let totalNI = niTaxBand1 + niTaxBand2;
-    let incomeTaxBand1 = 7500;
-    let incomeTaxBand2 = (gross - 37500) * 0.4;
-    let totalIT = incomeTaxBand1 + incomeTaxBand2;
-    let totalTax = totalNI + totalIT;
-    let yearlyGrossMinusTax = gross - totalTax;
-    let takeHomeMonthly = (gross - totalTax) / 12;
+    const niTaxBand1 = 4860;
+    const niTaxBand2 = (gross - 50000) * 0.02;
+    const totalNI = niTaxBand1 + niTaxBand2;
+    const incomeTaxBand1 = 7500;
+    const incomeTaxBand2 = (gross - 37500) * 0.4;
+    const totalIT = incomeTaxBand1 + incomeTaxBand2;
 
-    return (
-      "Your gross annual pay (in other words, pay before tax) is <strong>£" +
-      thous(gross.toFixed(2)) +
-      "</strong>." +
-      "<br>Your take home pay for the year is <strong>£" +
-      thous(yearlyGrossMinusTax.toFixed(2)) +
-      "</strong>, which works out at <strong>£" +
-      thous(takeHomeMonthly.toFixed(2)) +
-      "</strong> per month. " +
-      "<br>This means that over the year you will pay <strong>£" +
-      thous(totalIT) +
-      "</strong> in income tax, and <strong>£" +
-      thous(totalNI) +
-      "</strong> in National Insurance."
-    );
+    return outputTotal(totalIT, totalNI);
+
   } else if (gross > 150000) {
-    let niTaxBand1 = 4860;
-    let niTaxBand2 = (gross - 50000) * 0.02;
-    let totalNI = niTaxBand1 + niTaxBand2;
-    let incomeTaxBand1 = 7500;
-    let incomeTaxBand2 = 45000;
-    let incomeTaxBand3 = (gross - 150000) * 0.45;
-    let totalIT = incomeTaxBand1 + incomeTaxBand2 + incomeTaxBand3;
-    let totalTax = totalNI + totalIT;
-    let yearlyGrossMinusTax = gross - totalTax;
-    let takeHomeMonthly = (gross - totalTax) / 12;
+    const niTaxBand1 = 4860;
+    const niTaxBand2 = (gross - 50000) * 0.02;
+    const totalNI = niTaxBand1 + niTaxBand2;
+    const incomeTaxBand1 = 7500;
+    const incomeTaxBand2 = 45000;
+    const incomeTaxBand3 = (gross - 150000) * 0.45;
+    const totalIT = incomeTaxBand1 + incomeTaxBand2 + incomeTaxBand3;
 
-    return (
-      "Your gross annual pay (in other words, pay before tax) is <strong>£" +
-      thous(gross.toFixed(2)) +
-      "</strong>." +
-      "<br>Your take home pay for the year is <strong>£" +
-      thous(yearlyGrossMinusTax.toFixed(2)) +
-      "</strong>, which works out at <strong>£" +
-      thous(takeHomeMonthly.toFixed(2)) +
-      "</strong> per month. " +
-      "<br>This means that over the year you will pay <strong>£" +
-      thous(totalIT) +
-      "</strong> in income tax, and <strong>£" +
-      thous(totalNI) +
-      "</strong> in National Insurance."
-    );
-  } else if (gross <= 0) {
-    return "Error. Illegible amount entered.";
+    return outputTotal(totalIT, totalNI);
   } else {
     return "Error. Illegible amount entered.";
   }
@@ -193,6 +93,7 @@ function htmlEarnings() {
   event.preventDefault();
   document.getElementById("result").innerHTML = earnings(gross);
 }
+
 
 exports.thous = thous;
 exports.earnings = earnings;
